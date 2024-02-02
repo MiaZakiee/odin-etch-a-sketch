710ebfcecc7c1e1945a16dae2e4fde2642ba1dee
miazakiee <ninocabiltes@gmail.com>
2024-02-02 08:52:54 +0800

    Implemented dynamic pixel size

--- script.js
+++ script.js
@@ -1,42 +1,49 @@
-let isMouseDown = false;
-document.body.onmousedown = () => (isMouseDown = true)
-document.body.onmouseup = () => (isMouseDown = false)
+const board = document.querySelector('.board');
+const cell = document.querySelector('.cell');
+const DEFAULT_PIXEL_SIZE = 2;
 
-const createBoard = function () {
-    const board = document.querySelector('.board');
-    let ctr = 1;
-    for (let i = 0; i < 16; i++) {
-        let row = document.createElement('div');
-        row.classList.add('row');
-        for (let j = 0; j < 16; j++) {
-            let cell = document.createElement('div');
-            cell.classList.add('cell');
-            cell.id = 'cell' + ctr;
-            cell.draggable = false;
-
-            // saon mani oy
-            cell.addEventListener('mouseover', function () {
-                if (isMouseDown) {
-                    colorCell(cell.id);
-                } else {
-                    return;
-                }
-            });
-
-            row.appendChild(cell);
-            console.log("cell has been created");
-            ctr++;
-        }
-        board.appendChild(row);
-    }
-}
+let pixelSize = DEFAULT_PIXEL_SIZE;
 
 window.onload = function() {
     createBoard();
 }
 
+let isMouseDown = false;
+document.body.onmousedown = () => (isMouseDown = true)
+document.body.onmouseup = () => (isMouseDown = false)
+
+const createBoard = function () {
+    for (let i = 0; i < pixelSize * pixelSize; i++) {
+        let cell = document.createElement('div');
+        cell.classList.add('cell');
+        cell.id = 'cell_' + i;
+
+        cell.addEventListener('mouseover', function () {
+            if (isMouseDown) {
+                colorCell(cell.id);
+            }
+        });
+
+        cell.setAttribute("unselectable", "on");
+
+        let size = (500 / pixelSize) + "px";
+        cell.style.height = size;
+        cell.style.width = size;
+
+        board.appendChild(cell);
+    }
+    console.log("board has been created");
+}
+
 const colorCell = function (cellId) {
     const cell = document.querySelector('#' + cellId);
+    cell.classList.add('coloredCell');
     cell.style.backgroundColor = 'black';
-    console.log("Haha");
 };
+
+const clearAll = function () {
+    // looks o(1) pero o(n^2) yawa ka?
+    board.innerHTML = '';
+    createBoard();
+    console.log("Board cleared");
+}
